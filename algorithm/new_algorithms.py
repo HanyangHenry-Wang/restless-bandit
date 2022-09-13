c6fa5342ca764edf0f6567f425cc99d20b07586f
HanyangHenry-Wang <wanghanyang16@163.com>
2022-09-13 18:02:09 +0100

    update_20220913

--- algorithm/new_algorithms.py
+++ algorithm/new_algorithms.py
@@ -42,6 +42,7 @@ def general_DPTS(C,cov_control,*arm_list):
   
   K = len(arm_list)
   T = arm_list[0].T
+  sigma = arm_list[0].sigma
   
   choice = []
   arms = []
@@ -72,7 +73,7 @@ def general_DPTS(C,cov_control,*arm_list):
     arm_i = arm_list[i]
     kernel = GPy.kern.RBF(input_dim=1,variance=arm_i.variance,lengthscale=arm_i.lengthscale)   #using RBF kernel
     m = GPy.models.GPRegression(X_sample,Y_sample,kernel)
-    m.Gaussian_noise.variance.fix(0.1)
+    m.Gaussian_noise.variance.fix(sigma)
     GP_models.append(m)
     
     round_holder.append([])
@@ -125,10 +126,118 @@ def general_DPTS(C,cov_control,*arm_list):
 
     kernel = GPy.kern.RBF(input_dim=1,variance=arm_list[next_pull].variance,lengthscale=arm_list[next_pull].lengthscale)
     m = GPy.models.GPRegression(X_sample,Y_sample,kernel)
-    m.Gaussian_noise.variance.fix(0.1)
+    m.Gaussian_noise.variance.fix(sigma)
 
     GP_models[next_pull]=m
     
   Normal_regret = regret_holder/Normal
+  
+
+  return Normal_regret,choice
+
+
+
+
+
+def simulation_DPTS(C,simulation_num,*arm_list):
+  
+  K = len(arm_list)
+  T = arm_list[0].T
+  sigma = arm_list[0].sigma
+  
+  choice = []
+  arms = []
+  reward_paths = []
+  
+  for i in range(K):
+    arm_i = arm_list[i].arm_path
+    reward_i=arm_list[i].reward_path()
+    
+    arms.append(arm_i)
+    reward_paths.append(reward_i)
+    
+
+  max_reward = np.maximum.reduce(arms)
+  Normal=np.sum(np.abs(max_reward))
+  regret_holder=np.zeros(T)
+  
+  GP_models = [] #this is the holder of GP model of arm i
+  lengscale_holder = []  #the corresponding lengthscale  
+  round_holder= []    #this record the round that arm i is played
+  reward_holder= []    #this record the reward that arm i gives
+
+
+  X_sample=np.array([[0.0]])
+  Y_sample=np.array([[0.0]])
+  
+  for i in range (K):
+    arm_i = arm_list[i]
+    kernel = GPy.kern.RBF(input_dim=1,variance=arm_i.variance,lengthscale=arm_i.lengthscale)   #using RBF kernel
+    m = GPy.models.GPRegression(X_sample,Y_sample,kernel)
+    m.Gaussian_noise.variance.fix(sigma)
+    GP_models.append(m)
+    
+    round_holder.append([])
+    reward_holder.append([])
+
+    lengscale_holder.append(arm_i.lengthscale)
+
+
+  for t in range(T):
+
+
+    sample_TS=[]
+    future_step = int(min(lengscale_holder))
+    
+    
+    next_choice_holder = np.zeros(simulation_num)  
+    
+    for ii in range(simulation_num):
+      sample_TS=[]
+      for i in range(K):
+        X = np.array(range(t,min(t+future_step,T+1))).reshape(-1,1) 
+
+        temp = GP_models[i].posterior_samples_f(X,size=1).reshape(-1)
+        sample_TS.append(temp)
+
+      if t==0:   #determine the next pull
+        next_choice_holder[ii] = np.random.randint(0,K)   
+      else:
+        path = Assembly_Line_Scheduling(C, choice[-1], sample_TS)
+        next_choice_holder[ii] = path[0]
+        
+    next_choice_holder = next_choice_holder.astype(int)
+    counts = np.bincount(next_choice_holder)
+    next_pull = np.argmax(counts)
+    
+    if t >=1 :
+      if (next_pull != choice[-1]) : #check whether we switch the arm at time t
+        switch = 1
+      else:
+        switch = 0
+    else:
+      switch = 0
+
+    regret=max_reward[t]-arms[next_pull][t]    #calculate the regret
+    regret_holder[t]=regret+switch*C
+
+    reward= reward_paths[next_pull][t]     #obtain the reward
+
+    round_holder[next_pull].append(t)
+    reward_holder[next_pull].append(reward)
+    choice.append(next_pull)
+
+    X_sample= np.array(round_holder[next_pull]).reshape(-1,1)     
+    Y_sample=np.array(reward_holder[next_pull]).reshape(-1,1)
+    
+
+    kernel = GPy.kern.RBF(input_dim=1,variance=arm_list[next_pull].variance,lengthscale=arm_list[next_pull].lengthscale)
+    m = GPy.models.GPRegression(X_sample,Y_sample,kernel)
+    m.Gaussian_noise.variance.fix(sigma)
+
+    GP_models[next_pull]=m
+    
+  Normal_regret = regret_holder/Normal
+  
 
   return Normal_regret,choice
--- try.py
+++ try.py
@@ -1,13 +1,43 @@
+import os
 from algorithm.new_algorithms import * 
 from bandit_process.new_arms import *
 from matplotlib import pyplot as plt
-path0 = [0,5,5,0,0,0]
-path1 =  [1,0,0,10,10,10]
-path2 = [2,0,0,4,0,0]
-res = Assembly_Line_Scheduling(5,0,[path0,path1,path2])
-print(res)
-# a=GP_Arm(lengthscale=10, variance=5,sigma=0.1, T=200)
-# temp1 = a.arm_path
-# rewards=a.reward_path()
-# plt.scatter(200,rewards)
-# plt.show()
+
+
+C_holder = [0,1.5,30,40,60,80]
+
+
+for C in C_holder:
+  
+  regret_DPTS = []
+  choice_DPTS = []
+
+
+  N = 250
+  T = 400
+  
+  
+
+  for exp in range(N):
+
+
+    arm1=GP_Arm(lengthscale=6, variance=15,sigma=0.1, T=400)
+    arm2=GP_Arm(lengthscale=6, variance=15,sigma=0.1, T=400)
+    arm3=GP_Arm(lengthscale=18, variance=4,sigma=0.1, T=400)
+    arm4=GP_Arm(lengthscale=18, variance=4,sigma=0.1, T=400)
+ 
+    regret_holder1,choice_holder1 = simulation_DPTS(C,25, arm1,arm2,arm3,arm4)
+    
+    
+
+    regret_DPTS.append(regret_holder1)
+    choice_DPTS.append(choice_holder1)
+  
+    
+    
+
+  regret_record1 = np.array(regret_DPTS).reshape(N,T)
+  choice_DPTS = np.array(choice_DPTS)
+
+  np.savetxt('simulation_4arms_regret_DPTS_cost'+str(C)+'.csv', regret_record1, delimiter=',')
+  np.savetxt('simulation_4arms_choice_DPTS_cost'+str(C)+'.csv', choice_DPTS, delimiter=',')
